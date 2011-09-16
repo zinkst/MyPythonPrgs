ec242a9890ef3dc4a64c2eb6d0cd97712d611f76
zinks <zinks>
2011-09-16 19:26:32 +0000

    allow filelist input

--- DiaExifRenamer/DiaExifRenamer.py
+++ DiaExifRenamer/DiaExifRenamer.py
@@ -20,6 +20,10 @@ from xml.dom import Node
 import time
 import pyexiv2
 from stat import *
+import datetime
+import codecs
+import fileinput
+
 
 
 ############################################################################
@@ -74,42 +78,46 @@ def readConfigFromXML(configFileName):
                     tgtDirName = l2Node.getAttribute("value").encode(defaultEncoding)
                 if l2Node.nodeName == "toolName":
                     toolName = l2Node.getAttribute("value").encode(defaultEncoding)
-         
+                if l2Node.nodeName == "fileList":
+                    """ Zugriff auf den Wert des tags """
+                    #srcDirName = l2Node.getAttribute("value").encode(defaultEncoding)
+                    fileList=l2Node.firstChild.nodeValue.encode(defaultEncoding)
+                
         elif l1Node.nodeName == "generic":
             for l2Node in l1Node.childNodes:
                 if l2Node.nodeName == "toolOptions":
                     toolOptions = l2Node.getAttribute("value").encode(defaultEncoding)
                 if l2Node.nodeName == "fileFilter":
                     fileFilter = l2Node.getAttribute("value").encode(defaultEncoding)
-             
+                if l2Node.nodeName == "useList":
+                    useListStr = l2Node.getAttribute("value").encode(defaultEncoding)
+    
     logging.debug("srcDirName = %s" % srcDirName) 
     logging.debug("tgtDirName = %s" % tgtDirName) 
     logging.debug("toolName = %s" % toolName) 
     logging.debug("toolOptions = %s" % toolOptions) 
     logging.debug("fileFilter = %s" % fileFilter) 
+    logging.debug("fileList = %s" % fileList) 
+    logging.debug("useListStr = %s" % useListStr) 
+    if useListStr == "True" :
+      useList = True
+    else:  
+      useList = False
+       
+     
     
     inputParams["srcDirName"]=srcDirName.encode(defaultEncoding)
     inputParams["tgtDirName"]=tgtDirName.encode(defaultEncoding)
     inputParams["toolName"]=toolName.encode(defaultEncoding)
     inputParams["toolOptions"]=toolOptions.encode(defaultEncoding)
     inputParams["fileFilter"]=fileFilter.encode(defaultEncoding)
+    inputParams["fileList"]=fileList.encode(defaultEncoding)
+    inputParams["useList"]=useList
     
     return (inputParams)
 
 ###################################################################################################
 
-############################################################################
-def processFile(srcCompleteFileName, toolName, toolOptions):
-    fileInfo = {}
-    #fileInfo = parseFileNameStatic(srcCompleteFileName)
-    getMetadata(srcCompleteFileName,fileInfo)
-    #fileInfo = parseFileNameLastUnderscore(srcCompleteFileName,fileInfo)
-    parseFileNamePapierBilder(srcCompleteFileName, fileInfo)
-    setXMPdcSubject(srcCompleteFileName, fileInfo)
-    #exiftool -P -CreateDate='2009.05.06 06:47:00' -DateTimeDigitized='2009.05.06 06:47:00' -DateTimeOriginal=1995:05:24 -comment='Löwensteiner Berge' -UserComment='Löwensteiner Berge User' 1995_Nordeuropatour/1995A/1995A02_0524_Löwensteiner\ Berge.jpg
-    #setMetadata(srcCompleteFileName,fileInfo)
-    setFileDateToImageCreateDateTime(srcCompleteFileName, fileInfo)
-    
 ############################################################################
 def writeExifWithExiftool(srcCompleteFileName, toolName, toolOptions):    
     originalDateTag='-DateTimeOriginal="%s:%s:%s"' % (fileInfo["YEAR"],fileInfo["MONTH"],fileInfo["DAY"])
@@ -135,22 +143,29 @@ def getMetadata(srcCompleteFileName,fileInfo):
   fileInfo["EXIF_DATETIME"]=exifDateTimeString
 
 ############################################################################
-def setMetadata(srcCompleteFileName,fileInfo):
+def setDescriptionTags(srcCompleteFileName,fileInfo):
   metadata = pyexiv2.ImageMetadata(srcCompleteFileName)
   metadata.read()
-  try:
-    key1 = 'Exif.Photo.UserComment'
-    key2 = 'Exif.Image.ImageDescription'
-    key3 = 'Xmp.dc.subject'
-    value = fileInfo["FILMID"]
-    metadata[key1] = value
-    metadata[key2] = value
-    metadata.write()
-  except KeyError :
-    logging.ERROR("Error writing " + fileInfo["COMMENT"] + " to file " + srcCompleteFileName)       
+  keys = [ 'Exif.Photo.UserComment' , 'Exif.Image.ImageDescription' , 'Xmp.dc.description' , 'Iptc.Application2.Caption']
+  value = fileInfo["COMMENT"]
+  for curKey in keys:
+    try:
+      if (curKey == 'Xmp.dc.description'):
+        metadata[curKey] = {'x-default': value } 
+      elif (curKey == 'Iptc.Application2.Caption'):
+        metadata[curKey] = [ value ]
+      else:
+        metadata[curKey] = value 
+    except KeyError :
+      logging.ERROR("Error writing " + fileInfo["COMMENT"] + "in tag " + curKey + " to file " + srcCompleteFileName)       
   
+  
+  metadata.write(True)
+  #  logging.ERROR("Error writing " + fileInfo["COMMENT"] + " to file " + srcCompleteFileName)       
+    
+
 ############################################################################
-def setXMPdcSubject(srcCompleteFileName,fileInfo):
+def setKeywordTags(srcCompleteFileName,fileInfo):
   metadata = pyexiv2.ImageMetadata(srcCompleteFileName)
   metadata.read()
   key1 = 'Xmp.dc.subject'
@@ -163,7 +178,7 @@ def setXMPdcSubject(srcCompleteFileName,fileInfo):
   appendValuesList = []
   appendValuesList.append(fileInfo["FILMID"])
   appendValuesList.append("Hausbau")
-  appendValuesList.append("test1")
+  #appendValuesList.append("test1")
   
   for curValue in appendValuesList:
     if (curValue not in xmpSubjectList): 
@@ -171,16 +186,17 @@ def setXMPdcSubject(srcCompleteFileName,fileInfo):
       xmpSubjectList.append(curValue)
   try:  
     metadata[key1] = xmpSubjectList
-    metadata.write()
+    metadata.write(True)
   except KeyError :
       logging.ERROR("Error writing " + key_value + " to file " + srcCompleteFileName)       
 
 ############################################################################
-def setFileDateToImageCreateDateTime(srcCompleteFileName,fileInfo):
+def setFileDateToImageDigitizedDateTime(srcCompleteFileName,fileInfo):
   metadata = pyexiv2.ImageMetadata(srcCompleteFileName)
   metadata.read()
   try:
-    exifDateTimeTag = metadata['Exif.Image.DateTime']
+    #exifDateTimeTag = metadata['Exif.Image.DateTime']
+    exifDateTimeTag = metadata['Exif.Photo.DateTimeDigitized']
     st = os.stat(srcCompleteFileName)
     atime = st[ST_ATIME] #access time
 
@@ -191,6 +207,24 @@ def setFileDateToImageCreateDateTime(srcCompleteFileName,fileInfo):
     os.utime(srcCompleteFileName,(atime,exifDateTimetimestamp))
   except KeyError :
     "no dateteime tag in  " + srcCompleteFileName  
+
+############################################################################
+def setImageMetadataDateTimeFromFileNameTimeStamp(srcCompleteFileName,fileInfo):
+  metadata = pyexiv2.ImageMetadata(srcCompleteFileName)
+  metadata.read()
+  keys = [ 'Exif.Image.DateTime' , 'Exif.Photo.DateTimeDigitized' ,  'Xmp.xmp.CreateDate', 'Iptc.Application2.DateCreated',]
+  value = fileInfo["FILENAMEDT"]
+  for curKey in keys:
+    try:
+      if (curKey == 'Iptc.Application2.DateCreated'):
+        metadata[curKey] =  [value]
+#     elif (curKey == 'Iptc.Application2.Caption'):
+#        metadata[curKey] = [ value ]
+      else:
+        metadata[curKey] = value
+    except KeyError,TypeError :
+      logging.ERROR("Error writing " + fileInfo["FILENAMEDT"] + "in tag " + curKey + " to file " + srcCompleteFileName)       
+  metadata.write(True)
   
 
   
@@ -210,18 +244,29 @@ def parseFileNameStatic(srcCompleteFileName):
 
 ############################################################################
 def parseFileNameLastUnderscore(srcCompleteFileName,fileInfo):
+    # 20040320_181000_Kind1WandZuKind2_01.jpg
+    #splits = fileNameOnly.split('_')
+    #fileInfo["COMMENT"]=splits[2] 
+    
     # 1995A02_0524_Löwensteiner Berge.jpg
     fileNameOnly=os.path.basename(srcCompleteFileName)
     (id, sep, last) = fileNameOnly.rpartition('_')
     (comment, sep, extension) = last.rpartition('.')
     fileInfo["COMMENT"]=comment 
-    fileInfo["YEAR"]=id[0:4]
-    fileInfo["MONTH"]=id[8:10]
-    fileInfo["DAY"]=id[10:12]
+    fileInfo["YEAR"]=int(id[0:4])
+    fileInfo["MONTH"]=int(id[4:6])
+    fileInfo["DAY"]=int(id[6:8])
+    fileInfo["HOUR"]=int(id[9:11])
+    fileInfo["MINUTE"]=int(id[11:13])
+    fileInfo["SECOND"]=int(id[13:15])
+    fileNameDT = datetime.datetime(fileInfo["YEAR"],fileInfo["MONTH"],fileInfo["DAY"],fileInfo["HOUR"],fileInfo["MINUTE"],fileInfo["SECOND"])
+    fileInfo["FILENAMEDT"]=fileNameDT
     fileCreationDate=os.stat(srcCompleteFileName)[ST_CTIME]
     fileInfo["FILEDATE"]=time.strftime('%Y:%m:%d',time.localtime(fileCreationDate))   
+    
     logging.info(fileInfo)
     return fileInfo
+
 
 ############################################################################
 def parseFileNamePapierBilder(srcCompleteFileName,fileInfo):
@@ -229,11 +274,48 @@ def parseFileNamePapierBilder(srcCompleteFileName,fileInfo):
     (filmid, sep, extension) = fileNameOnly.rpartition('.')
     fileInfo["FILMID"]=filmid
     logging.info(fileInfo)
+
+############################################################################
+def walkList(inputParams):
+  listName=inputParams["fileList"]
+  logging.info("listName = "+ listName)
+  # file:///home/zinks/Photos/Hausbau/digital/200410/20050522_183022_MartinHolzdeckeEG.jpg
+  
+  for line in fileinput.input(listName):
+    if line.startswith("file://"):
+      line = line[7:].rstrip()
+    processFile(line,inputParams)
+    
+############################################################################
+def walkDir(inputParams):
+  for Verz, VerzList, DateiListe in os.walk (inputParams["srcDirName"]):
+    logging.debug(" VerzList = " + str(VerzList) )
+    logging.debug(" DateiListe = " + str(DateiListe))
+    for Datei in sorted(DateiListe):
+      srcCompleteFileName  = os.path.join(Verz,Datei)
+      logging.debug(" srcCompleteFileName  = " + srcCompleteFileName)
+      if fnmatch.fnmatch(srcCompleteFileName, inputParams["fileFilter"]):
+        #tgtCompleteFileName = findTGTFileName(srcCompleteFileName, inputParams["srcDirName"],inputParams["tgtDirName"])
+        processFile(srcCompleteFileName, inputParams)
+
+############################################################################
+def processFile(srcCompleteFileName, inputParams):
+    logging.debug("Processing file "+ srcCompleteFileName)
+    fileInfo = {}
+    #fileInfo = parseFileNameStatic(srcCompleteFileName)
+    #getMetadata(srcCompleteFileName,fileInfo)
+    parseFileNameLastUnderscore(srcCompleteFileName,fileInfo)
+    #parseFileNamePapierBilder(srcCompleteFileName, fileInfo)
+    #setDescriptionTags(srcCompleteFileName, fileInfo)
+    #exiftool -P -CreateDate='2009.05.06 06:47:00' -DateTimeDigitized='2009.05.06 06:47:00' -DateTimeOriginal=1995:05:24 -comment='Löwensteiner Berge' -UserComment='Löwensteiner Berge User' 1995_Nordeuropatour/1995A/1995A02_0524_Löwensteiner\ Berge.jpg
+    #setKeywordTags(srcCompleteFileName,fileInfo)
+    setFileDateToImageDigitizedDateTime(srcCompleteFileName, fileInfo)
+    #setImageMetadataDateTimeFromFileNameTimeStamp(srcCompleteFileName, fileInfo)
+
+
 ############################################################################
 # main starts here
 # global variables
-
-
 rootLogger = initLogger()
 if sys.platform == "win32":
   defaultEncoding="latin1"
@@ -249,17 +331,13 @@ else:
 
 inputParams={}
 
-inputParms = readConfigFromXML(configFileName)
+inputParams = readConfigFromXML(configFileName)
 
-for Verz, VerzList, DateiListe in os.walk (inputParams["srcDirName"]):
-    logging.debug(" VerzList = " + str(VerzList) )
-    logging.debug(" DateiListe = " + str(DateiListe))
-    for Datei in sorted(DateiListe):
-        srcCompleteFileName  = os.path.join(Verz,Datei)
-        logging.debug(" srcCompleteFileName  = " + srcCompleteFileName)
-        if fnmatch.fnmatch(srcCompleteFileName, inputParams["fileFilter"]):
-            #tgtCompleteFileName = findTGTFileName(srcCompleteFileName, inputParams["srcDirName"],inputParams["tgtDirName"])
-            processFile(srcCompleteFileName, inputParams["toolName"],inputParams["toolOptions"])
-            
-print ("successfully transfered %s " % inputParams["srcDirName"])
+if inputParams["useList"]:
+  walkList(inputParams) 
+  print ("successfully transfered %s " % inputParams["srcDirName"])
+else:
+  walkDir(inputParams) 
+  print ("successfully transfered %s " % inputParams["fileList"])
+
 
